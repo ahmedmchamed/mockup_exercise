1f9ce509d455840325a8118dc6c4e9fb12411d60
Ahmed Hamed <tunashark@gmail.com>
2020-10-12 16:42:56 +0100

    meetings list fetche

--- src/containers/main.js
+++ src/containers/main.js
@@ -6,8 +6,31 @@ class Main extends Component {
         constructor(props) {
             super(props)
             this.state = {
-
+                meetingsList: []
             }
+
+            this.fetchMeetingsList = this.fetchMeetingsList.bind(this);
+        }
+
+        fetchMeetingsList() {
+            const meetingsUrl = "https://coding-test.ajenta.io/meetings";
+
+            fetch(meetingsUrl)
+            .then(res => res.json())
+            .then(meetingsList => {
+                this.setState(prevState => {
+                    return {
+                        meetingsList: meetingsList
+                    }
+                }, () => {
+                    console.log(this.state.meetingsList)
+                })
+            })
+            .catch(err => console.error(err))
+        }
+
+        componentDidMount() {
+            this.fetchMeetingsList();
         }
 
         render() {
